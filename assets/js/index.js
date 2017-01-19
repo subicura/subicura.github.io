30afdd343eba486d9cb9789e2234016e96b1bec2
subicura <subicura@subicura.com>
2017-01-20 02:53:35 +0900

    Add docker guide for beginners

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,15 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-        
+
         // Calculates Reading Time
-        $('.post-content').readingTime({
+        var txt = $('<div></div>').hide().text($('.post-content p, .post-content li').text()).appendTo($('body'));
+        txt.readingTime({
             readingTimeTarget: '.post-reading-time',
             wordCountTarget: '.post-word-count',
+            wordsPerMinute: 200,
         });
-        
+
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
@@ -23,7 +25,7 @@
               $(this).wrap('<figure class="image"></figure>')
               .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
-        
+
     });
 
 }(jQuery));
